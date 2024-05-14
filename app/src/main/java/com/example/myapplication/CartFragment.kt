db3d358323e277f976a4c3d95b0c10ca6f3b02a4
21B030702 <a_nusip@kbtu.kz>
2024-05-15 00:43:43 +0500

    "third commit"

--- app/src/main/java/com/example/myapplication/CartFragment.kt
+++ app/src/main/java/com/example/myapplication/CartFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -33,8 +35,11 @@ class CartFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cart, container, false)
+        val view = inflater.inflate(R.layout.fragment_cart, container, false)
+        val recyclerView: RecyclerView = view.findViewById(R.id.recycler_view_cart)
+        recyclerView.layoutManager = LinearLayoutManager(context)
+        recyclerView.adapter = CartAdapter(listOf("Item 1", "Item 2", "Item 3"))
+        return view
     }
 
     companion object {
