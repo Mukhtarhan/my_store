b9fe90c6921b9b5876557753548c9bc07218c570
zhanel01 <zhumanovazhanel@mail.ru>
2024-05-17 17:29:43 +0600

    commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,21 +39,26 @@ android {
 }
 
 dependencies {
+    implementation("com.google.android.gms:play-services-analytics-impl:18.0.4")
     val navVersion = "2.7.7"
     val roomVersion = "2.6.1"
-
-
-    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
+    implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.github.bumptech.glide:glide:4.16.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-
     implementation("androidx.navigation:navigation-fragment-ktx:$navVersion")
     implementation("androidx.navigation:navigation-ui-ktx:$navVersion")
-
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.0")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.8.0")
+    implementation("androidx.arch.lifecycle:extensions:1.1.1")
+    implementation("androidx.room:room-runtime:$roomVersion")
 
 
 }
